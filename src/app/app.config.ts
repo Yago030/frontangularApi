7cbdaf753b18e9c13d5fe6991378a035bab39ea6
Santiago Bergerat <santiagobergerat@gmail.com>
2024-11-27 09:33:32 -0300

    primer commit del api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClient, HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './custom/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+  provideAnimationsAsync(),
+  importProvidersFrom(HttpClientModule),
+  provideHttpClient(withInterceptors([authInterceptor])) //con esto avisamos que la vamos a interceptar con ese interceptor
+]
 };
